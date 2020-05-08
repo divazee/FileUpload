09937a699d3ef67ff9691df53162cec3cf3fbd1d
Zeeeey <ahumibe.ezinne@tizeti.com>
2020-05-08 13:20:42 +0100

    some stuff... can revert to previous commit

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,43 +1,43 @@
-//id - 157499151735-2a2rm0jv17iksp0he6prllimq4seivk5.apps.googleusercontent.com
-// secret - z96XZcYxVw_voFb3MQHb36iY
-
 import React, { useState } from 'react';
 import axios from 'axios'
+import path from 'path'
+
+
+const dirPath = path.join(__dirname)
+
 
     const Button = () => { 
         const [ file, setFile ] = useState('')
         const [ filename, setFilename ] = useState('Choose File')
 
         const chooseFile = (e) => {
-            console.log("object2")
             setFile(e.target.files[0])
             setFilename(e.target.files[0].name)
-            // console.log("object:::::", __dirname)
         }
 
         const handleUpload = async (e) => {
             console.log("object")
-            // const formData = new FormData();
-            // formData.append('file', file);
+            const formData = new FormData();
+            formData.append('file', file);
 
             console.log("file selected", file)
 
-            // try {
-                // const result = await axios({
-                //     method: 'POST',
-                //     url: `https://www.googleapis.com/upload/drive/v3/files?uploadType=media`,
-                //     file,
+            try {
+                const result = await axios({
+                    method: 'POST',
+                    url: `https://www.googleapis.com/upload/drive/v3/files?uploadType=media`,
+                    formData,
                 //     headers: {
                 //         // 'Authorization': `Bearer ${}`,
                 //         'Content-Type': 'image/jpeg',
                 //         'Content-Length':  500000
                 //     }
-                // })
-                // console.log("object33:::", result)
-            // } catch (e) {
-            //     console.log("error::", e)
+                })
+                console.log("object33:::", result)
+            } catch (e) {
+                console.log("error::", e)
 
-            // }
+            }
             
         }
     
